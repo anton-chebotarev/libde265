fdc0609942e5b1b6057c7cc92f892175e1a37db5
Anton Chebotarev <anton.chebotarev@materialise.kiev.ua>
2020-07-30 15:02:56 +0300

    js build script

--- buildjs.sh
+++ buildjs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 export LIBDE265_VERSION=1.0.4
 
+git restore .
+git pull
 chmod 777 ./configure
+
 emconfigure ./configure --disable-sse --disable-dec265 --disable-enc265 --disable-sherlock265
 emmake make
 
